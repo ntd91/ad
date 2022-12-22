9cc6285ed6e6edbd0e9a6e70bf9f0605094f0b8e
ntd91 <84556033+ntd91@users.noreply.github.com>
2022-12-22 11:14:59 +0800

    Add files via upload

---
+++ reget.js
@@ -0,0 +1,31 @@
+reget = (() => {
+    const reget = () => {
+        fetch("/voslot-ims/api/v1/thirdpartypayments?", {
+            "headers": {
+              "authorization": localStorage.getItem("token").replaceAll('"', ''),
+            },
+            "method": "GET",
+          }).then((response) => response.json())
+          .then((data) => {
+            var limit = 100000
+            var result = ""
+            data.forEach(d => {
+              let maxdailyamt = d.currencies[0].maxdailyamt
+              let maxtotalamt = d.currencies[0].maxtotalamt
+              let dailyamt = d.currencies[0].dailyamt
+              let totalamt = d.currencies[0].totalamt
+              let daily = maxdailyamt - dailyamt
+              let total = maxtotalamt - totalamt
+              if (daily < limit || total < limit) {
+                result += d.thirdpartypaymentcode + ': ' + d.thirdpartypaymentdisplayname['en-US'] + '每日or總收剩餘小於: ' + limit + '\r\n'
+              }
+            })
+            if (result != '') {
+              alert(result)
+            }
+          });
+      }
+      setInterval(() => {
+        reget()
+      }, 900000);
+})();
